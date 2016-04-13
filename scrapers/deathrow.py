0f90044140e6070d48ab9c1f38a82ab873429024
Brandi <Brandi@Brandis-MacBook-Pro.local>
2016-04-13 12:55:37 -0400

    executions.csv

---
+++ scrapers/deathrow.py
@@ -0,0 +1,27 @@
+import csv
+import requests
+from BeautifulSoup import BeautifulSoup
+
+url ='http://www.tdcj.state.tx.us/death_row/dr_scheduled_executions.html'
+
+response = requests.get(url)
+html = response.content
+
+soup = BeautifulSoup(html)
+
+table = soup.findAll('table')[0]
+
+list_of_rows = []
+for row in table.findAll('tr')[1:]:
+    list_of_cells = []
+    for cell in row.findAll('td'):
+        list_of_cells.append(cell.text)
+        if cell.find('a'):
+            list_of_cells.append("http://www.tdcj.state.tx.us/death_row/dr_scheduled_executions.html" + cell.find('a')['href'])
+    list_of_rows.append(list_of_cells)
+
+outfile = open("./executions.csv", "wb")
+writer = csv.writer(outfile)
+writer.writerow(['scheduled_execution','url','last_name','first_name','tdcj_number','DOB','race','date_received','county'])
+writer.writerows(list_of_rows)
+              
